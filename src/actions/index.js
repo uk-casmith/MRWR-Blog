8e1aa02801d698a1939db95de2658a4cfce0bd68
Cameron Smith <gitcontrol@outlook.com>
2020-01-30 23:26:36 +0000

    completed course section

---
+++ src/actions/index.js
@@ -0,0 +1,52 @@
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  /**
+  // get unique users ids.
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+
+  // fetch only unique users by dispatching.
+  userIds.forEach(id => dispatch(fetchUser(id)));
+  */
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value(); // we MUST do value otherwise it won't execute!
+};
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({
+        type: 'FETCH_POSTS',
+        payload: response.data
+    })
+};
+
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({
+      type: 'FETCH_USER',
+      payload: response.data
+  })
+};
+
+/**
+export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({
+      type: 'FETCH_USER',
+      payload: response.data
+  })
+});
+*/
